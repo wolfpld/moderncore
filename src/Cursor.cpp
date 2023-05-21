82930f88a705d876f34fa27058c7ddcf934e8821
Bartosz Taudul <wolf@nereid.pl>
2023-05-21 13:46:17 +0200

    Hook cursor events to callbacks.

--- src/Cursor.cpp
+++ src/Cursor.cpp
@@ -7,6 +7,8 @@ extern "C" {
 #include <wlr/types/wlr_xcursor_manager.h>
 };
 
+static Cursor* s_instance;
+
 Cursor::Cursor()
     : m_cursor( wlr_cursor_create() )
     , m_manager( wlr_xcursor_manager_create( nullptr, 24 ) )
@@ -14,11 +16,53 @@ Cursor::Cursor()
     CheckPanic( m_cursor, "Failed to create wlr_cursor!" );
     CheckPanic( m_manager, "Failed to create wlr_xcursor_manager!" );
 
+    CheckPanic( !s_instance, "Creating a second instance of Cursor!" );
+    s_instance = this;
+
     wlr_xcursor_manager_load( m_manager, 1 );
+
+    m_motion.notify = []( struct wl_listener*, void* data ){ s_instance->Motion( (struct wlr_pointer_motion_event*)data ); };
+    m_motionAbsolute.notify = []( struct wl_listener*, void* data ){ s_instance->MotionAbsolute( (struct wlr_pointer_motion_absolute_event*)data ); };
+    m_button.notify = []( struct wl_listener*, void* data ){ s_instance->Button( (struct wlr_pointer_button_event*)data ); };
+    m_axis.notify = []( struct wl_listener*, void* data ){ s_instance->Axis( (struct wlr_pointer_axis_event*)data ); };
+    m_frame.notify = []( struct wl_listener*, void* data ){ s_instance->Frame(); };
+
+    wl_signal_add( &m_cursor->events.motion, &m_motion );
+    wl_signal_add( &m_cursor->events.motion_absolute, &m_motionAbsolute );
+    wl_signal_add( &m_cursor->events.button, &m_button );
+    wl_signal_add( &m_cursor->events.axis, &m_axis );
+    wl_signal_add( &m_cursor->events.frame, &m_frame );
 }
 
 Cursor::~Cursor()
 {
+    wl_list_remove( &m_motion.link );
+    wl_list_remove( &m_motionAbsolute.link );
+    wl_list_remove( &m_button.link );
+    wl_list_remove( &m_axis.link );
+    wl_list_remove( &m_frame.link );
+
     wlr_xcursor_manager_destroy( m_manager );
     wlr_cursor_destroy( m_cursor );
+    s_instance = nullptr;
+}
+
+void Cursor::Motion( struct wlr_pointer_motion_event* ev )
+{
+}
+
+void Cursor::MotionAbsolute( struct wlr_pointer_motion_absolute_event* ev )
+{
+}
+
+void Cursor::Button( struct wlr_pointer_button_event* ev )
+{
+}
+
+void Cursor::Axis( struct wlr_pointer_axis_event* ev )
+{
+}
+
+void Cursor::Frame()
+{
 }
--- src/Cursor.hpp
+++ src/Cursor.hpp
@@ -1,9 +1,15 @@
 #ifndef __CURSOR_HPP__
 #define __CURSOR_HPP__
 
+#include <wayland-server-core.h>
+
 extern "C" {
     struct wlr_cursor;
     struct wlr_xcursor_manager;
+    struct wlr_pointer_motion_event;
+    struct wlr_pointer_motion_absolute_event;
+    struct wlr_pointer_button_event;
+    struct wlr_pointer_axis_event;
 };
 
 class Cursor
@@ -12,9 +18,21 @@ public:
     Cursor();
     ~Cursor();
 
+    void Motion( struct wlr_pointer_motion_event* ev );
+    void MotionAbsolute( struct wlr_pointer_motion_absolute_event* ev );
+    void Button( struct wlr_pointer_button_event* ev );
+    void Axis( struct wlr_pointer_axis_event* ev );
+    void Frame();
+
 private:
     struct wlr_cursor* m_cursor;
     struct wlr_xcursor_manager* m_manager;
+
+    struct wl_listener m_motion;
+    struct wl_listener m_motionAbsolute;
+    struct wl_listener m_button;
+    struct wl_listener m_axis;
+    struct wl_listener m_frame;
 };
 
 #endif

915237c776f9c69d617cf81bf355267358f8e2ff
Bartosz Taudul <wolf@nereid.pl>
2023-05-22 01:13:03 +0200

    Add ANSI color code macros.

---
+++ src/Ansi.hpp
@@ -0,0 +1,14 @@
+#ifndef __ANSI_HPP__
+#define __ANSI_HPP__
+
+#define ANSI_RESET "\033[0m"
+#define ANSI_BOLD "\033[1m"
+#define ANSI_BLACK "\033[30m"
+#define ANSI_RED "\033[31m"
+#define ANSI_GREEN "\033[32m"
+#define ANSI_YELLOW "\033[33m"
+#define ANSI_MAGENTA "\033[35m"
+#define ANSI_CYAN "\033[36m"
+#define ANSI_ERASE_LINE "\033[2K"
+
+#endif

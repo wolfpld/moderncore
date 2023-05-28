99869a9ef96eaa024e1c4a3b644712138a06ec6b
Bartosz Taudul <wolf@nereid.pl>
2023-05-28 15:17:39 +0200

    Allow changing unordered_flat_{map,set} to STL for debug purposes.
    The robin hood variant of the map and set has no debug support in the
    tools such as debuggers or IDEs. However, the standard implementation
    of the map and set would typically have support for introspection of
    its contents.
    
    The implementation details of both variants obviously is completely
    different, but the interfaces are generally compatible.

--- src/util/RobinHood.hpp
+++ src/util/RobinHood.hpp
@@ -1,6 +1,27 @@
 #ifndef __ROBINHOOD_HPP__
 #define __ROBINHOOD_HPP__
 
+#if 0
+
+// For debug purposes only
+
+#include <unordered_map>
+#include <unordered_set>
+
+template<typename _Key, typename _Tp,
+    typename _Hash = std::hash<_Key>,
+    typename _Pred = std::equal_to<_Key>,
+    typename _Alloc = std::allocator<std::pair<const _Key, _Tp>>>
+using unordered_flat_map = std::unordered_map<_Key, _Tp, _Hash, _Pred, _Alloc>;
+
+template<typename _Value,
+    typename _Hash = std::hash<_Value>,
+    typename _Pred = std::equal_to<_Value>,
+    typename _Alloc = std::allocator<_Value>>
+using unordered_flat_set = std::unordered_set<_Value, _Hash, _Pred, _Alloc>;
+
+#else
+
 #include "../../contrib/robin_hood.h"
 
 template <typename Key, typename T, typename Hash = robin_hood::hash<Key>,
@@ -10,5 +31,7 @@ using unordered_flat_map = robin_hood::unordered_flat_map<Key, T, Hash, KeyEqual
 template <typename Key, typename Hash = robin_hood::hash<Key>, typename KeyEqual = std::equal_to<Key>,
           size_t MaxLoadFactor100 = 80>
 using unordered_flat_set = robin_hood::unordered_flat_set<Key, Hash, KeyEqual, MaxLoadFactor100>;
+
+#endif
 
 #endif

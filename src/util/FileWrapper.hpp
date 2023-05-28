97210fa2b6a5cfdc29932cacfe735dcf244b4356
Bartosz Taudul <wolf@nereid.pl>
2023-05-28 13:35:48 +0200

    Add RAII wrapper over FILE*.

---
+++ src/util/FileWrapper.hpp
@@ -0,0 +1,31 @@
+#ifndef __FILEWRAPPER_HPP__
+#define __FILEWRAPPER_HPP__
+
+#include <stdio.h>
+
+class FileWrapper
+{
+public:
+    FileWrapper( const char* fn, const char* mode )
+        : m_file( fopen( fn, mode ) )
+    {
+    }
+
+    ~FileWrapper()
+    {
+        if( m_file ) fclose( m_file );
+    }
+
+    bool Read( void* dst, size_t size )
+    {
+        return fread( dst, 1, size, m_file ) == size;
+    }
+
+    operator bool() const { return m_file != nullptr; }
+    operator FILE*() { return m_file; }
+
+private:
+    FILE* m_file;
+};
+
+#endif

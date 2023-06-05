d10037a91111c33bf7b4b068606949dd718720cc
Bartosz Taudul <wolf@nereid.pl>
2023-06-05 23:34:16 +0200

    Implement custom logging infrastructure.

--- src/plumbing/Logs.cpp
+++ src/plumbing/Logs.cpp
@@ -28,3 +28,25 @@ void SetupLogging()
 {
     wlr_log_init( WLR_DEBUG, LogCallback );
 }
+
+void MCoreLogMessage( LogLevel level, const char* fileName, size_t line, const char *fmt, ... )
+{
+    va_list args;
+    va_start( args, fmt );
+
+    switch( level )
+    {
+    case LogLevel::Debug: printf( ANSI_BOLD ANSI_BLACK "[DEBUG] " ); break;
+    case LogLevel::Info: printf( "[INFO] " ); break;
+    case LogLevel::Warning: printf( ANSI_BOLD ANSI_YELLOW "[WARNING] " ); break;
+    case LogLevel::Error: printf( ANSI_BOLD ANSI_RED "[ERROR] " ); break;
+    case LogLevel::Fatal: printf( ANSI_BOLD ANSI_MAGENTA "[FATAL] " ); break;
+    default: assert( false ); break;
+    }
+
+    printf( "[%s:%zu] ", fileName, line );
+    vprintf( fmt, args );
+    printf( ANSI_RESET "\n" );
+    fflush( stdout );
+    va_end( args );
+}
--- src/plumbing/Logs.hpp
+++ src/plumbing/Logs.hpp
@@ -1,6 +1,20 @@
 #ifndef __LOGS_HPP__
 #define __LOGS_HPP__
 
+#include <stddef.h>
+
+enum class LogLevel
+{
+    Debug,
+    Info,
+    Warning,
+    Error,
+    Fatal
+};
+
 void SetupLogging();
+void MCoreLogMessage( LogLevel level, const char* fileName, size_t line, const char* fmt, ... );
+
+#define mclog(level, fmt, ...) MCoreLogMessage( level, __FILE__, __LINE__, fmt, ##__VA_ARGS__ )
 
 #endif

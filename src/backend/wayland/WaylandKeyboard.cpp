b1edd65e40757b4c8ea3b7f232b9c4d27f93670c
Bartosz Taudul <wolf@nereid.pl>
2023-06-08 00:52:22 +0200

    Add wayland keyboard stub.

---
+++ src/backend/wayland/WaylandKeyboard.cpp
@@ -0,0 +1,46 @@
+#include "WaylandKeyboard.hpp"
+#include "WaylandMethod.hpp"
+
+WaylandKeyboard::WaylandKeyboard( wl_keyboard *keyboard )
+    : m_keyboard( keyboard )
+{
+    static constexpr struct wl_keyboard_listener keyboardListener = {
+        .keymap = Method( WaylandKeyboard, KeyboardKeymap ),
+        .enter = Method( WaylandKeyboard, KeyboardEnter ),
+        .leave = Method( WaylandKeyboard, KeyboardLeave ),
+        .key = Method( WaylandKeyboard, KeyboardKey ),
+        .modifiers = Method( WaylandKeyboard, KeyboardModifiers ),
+        .repeat_info = Method( WaylandKeyboard, KeyboardRepeatInfo )
+    };
+
+    wl_keyboard_add_listener( m_keyboard, &keyboardListener, this );
+}
+
+WaylandKeyboard::~WaylandKeyboard()
+{
+    wl_keyboard_destroy( m_keyboard );
+}
+
+void WaylandKeyboard::KeyboardKeymap( wl_keyboard* kbd, uint32_t format, int32_t fd, uint32_t size )
+{
+}
+
+void WaylandKeyboard::KeyboardEnter( wl_keyboard* kbd, uint32_t serial, wl_surface* surf, wl_array* keys )
+{
+}
+
+void WaylandKeyboard::KeyboardLeave( wl_keyboard* kbd, uint32_t serial, wl_surface* surf )
+{
+}
+
+void WaylandKeyboard::KeyboardKey( wl_keyboard* kbd, uint32_t serial, uint32_t time, uint32_t key, uint32_t state )
+{
+}
+
+void WaylandKeyboard::KeyboardModifiers( wl_keyboard* kbd, uint32_t serial, uint32_t mods_depressed, uint32_t mods_latched, uint32_t mods_locked, uint32_t group )
+{
+}
+
+void WaylandKeyboard::KeyboardRepeatInfo( wl_keyboard* kbd, int32_t rate, int32_t delay )
+{
+}
---
+++ src/backend/wayland/WaylandKeyboard.hpp
@@ -0,0 +1,23 @@
+#ifndef __WAYLANDKEYBOARD_HPP__
+#define __WAYLANDKEYBOARD_HPP__
+
+#include <wayland-client.h>
+
+class WaylandKeyboard
+{
+public:
+    explicit WaylandKeyboard( wl_keyboard* keyboard );
+    ~WaylandKeyboard();
+
+private:
+    wl_keyboard* m_keyboard;
+
+    void KeyboardKeymap( wl_keyboard* kbd, uint32_t format, int32_t fd, uint32_t size );
+    void KeyboardEnter( wl_keyboard* kbd, uint32_t serial, wl_surface* surf, wl_array* keys );
+    void KeyboardLeave( wl_keyboard* kbd, uint32_t serial, wl_surface* surf );
+    void KeyboardKey( wl_keyboard* kbd, uint32_t serial, uint32_t time, uint32_t key, uint32_t state );
+    void KeyboardModifiers( wl_keyboard* kbd, uint32_t serial, uint32_t mods_depressed, uint32_t mods_latched, uint32_t mods_locked, uint32_t group );
+    void KeyboardRepeatInfo( wl_keyboard* kbd, int32_t rate, int32_t delay );
+};
+
+#endif

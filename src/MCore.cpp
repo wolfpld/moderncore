10a47637871b296033e684cf2857f3ddc88f8b04
Bartosz Taudul <wolf@nereid.pl>
2023-06-08 00:57:04 +0200

    Custom backend test.

--- src/MCore.cpp
+++ src/MCore.cpp
@@ -1,9 +1,18 @@
 #include "Server.hpp"
 #include "util/Logs.hpp"
 
+#include "backend/wayland/BackendWayland.hpp"
+#include "vulkan/VlkInstance.hpp"
+
 int main()
 {
     SetupLogging();
+
+    {
+        VlkInstance vk;
+        BackendWayland ww;
+        ww.Run();
+    }
 
     Server server;
 

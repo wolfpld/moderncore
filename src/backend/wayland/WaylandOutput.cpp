9cfe8d8483ef7565f12b68657a9d660a0942e2da
Bartosz Taudul <wolf@nereid.pl>
2023-06-08 00:55:13 +0200

    Add wayland output stub.

---
+++ src/backend/wayland/WaylandOutput.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#include "WaylandMethod.hpp"
+#include "WaylandOutput.hpp"
+
+WaylandOutput::WaylandOutput( wl_output *output, std::function<void()> onOutput )
+    : m_onOutput( std::move( onOutput ) )
+    , m_output( output )
+    , m_scale( 1 )
+{
+    static constexpr struct wl_output_listener outputListener = {
+        .geometry = Method( WaylandOutput, OutputGeometry ),
+        .mode = Method( WaylandOutput, OutputMode ),
+        .done = Method( WaylandOutput, OutputDone ),
+        .scale = Method( WaylandOutput, OutputScale ),
+        .name = Method( WaylandOutput, OutputName ),
+        .description = Method( WaylandOutput, OutputDescription )
+    };
+
+    wl_output_add_listener( m_output, &outputListener, this );
+}
+
+WaylandOutput::~WaylandOutput()
+{
+    wl_output_destroy( m_output );
+}
+
+void WaylandOutput::OutputGeometry( wl_output* output, int32_t x, int32_t y, int32_t phys_w, int32_t phys_h, int32_t subpixel, const char* make, const char* model, int32_t transform )
+{
+}
+
+void WaylandOutput::OutputMode( wl_output* output, uint32_t flags, int32_t width, int32_t height, int32_t refresh )
+{
+}
+
+void WaylandOutput::OutputDone( wl_output* output )
+{
+    m_onOutput();
+}
+
+void WaylandOutput::OutputScale( wl_output* output, int32_t scale )
+{
+    m_scale = scale;
+}
+
+void WaylandOutput::OutputName( wl_output* output, const char* name )
+{
+}
+
+void WaylandOutput::OutputDescription( wl_output* output, const char* description )
+{
+}
---
+++ src/backend/wayland/WaylandOutput.hpp
@@ -0,0 +1,29 @@
+#ifndef __WAYLANDOUTPUT_HPP__
+#define __WAYLANDOUTPUT_HPP__
+
+#include <functional>
+#include <stdint.h>
+#include <wayland-client.h>
+
+class WaylandOutput
+{
+public:
+    WaylandOutput( wl_output* output, std::function<void()> onOutput );
+    ~WaylandOutput();
+
+    [[nodiscard]] int32_t GetScale() const { return m_scale; }
+
+private:
+    void OutputGeometry( wl_output* output, int32_t x, int32_t y, int32_t phys_w, int32_t phys_h, int32_t subpixel, const char* make, const char* model, int32_t transform );
+    void OutputMode( wl_output* output, uint32_t flags, int32_t width, int32_t height, int32_t refresh );
+    void OutputDone( wl_output* output );
+    void OutputScale( wl_output* output, int32_t scale );
+    void OutputName( wl_output* output, const char* name );
+    void OutputDescription( wl_output* output, const char* description );
+
+    std::function<void()> m_onOutput;
+    wl_output* m_output;
+    int32_t m_scale;
+};
+
+#endif

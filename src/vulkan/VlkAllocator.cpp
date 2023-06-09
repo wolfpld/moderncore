3eb8291bad863f8e2877e3e6f774f161c9552452
Bartosz Taudul <wolf@nereid.pl>
2023-06-09 22:03:36 +0200

    Compile VMA implementation.

---
+++ src/vulkan/VlkAllocator.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define VMA_IMPLEMENTATION
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnullability-completeness"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#include "../../contrib/vk_mem_alloc.h"
+#pragma clang diagnostic pop

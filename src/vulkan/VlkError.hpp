5a1acf066738cb759215df03e5cf920ada79259a
Bartosz Taudul <wolf@nereid.pl>
2023-06-15 00:37:56 +0200

    Abort on VkVerify fails.

--- src/vulkan/VlkError.hpp
+++ src/vulkan/VlkError.hpp
@@ -1,6 +1,7 @@
 #ifndef __VLKERROR_HPP__
 #define __VLKERROR_HPP__
 
+#include <stdlib.h>
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_enum_string_helper.h>
 
@@ -11,6 +12,7 @@
         if( (res) != VK_SUCCESS ) \
         { \
             mclog( LogLevel::Fatal, "VkVerify fail: %i (%s)", (res), string_VkResult( res ) ); \
+            abort(); \
         } \
     }
 

bba2a93380bf43ed98109127ae5124af8c459553
Bartosz Taudul <wolf@nereid.pl>
2023-10-18 23:19:50 +0200

    Add test for Panic.

---
+++ tests/PanicTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "../src/util/Panic.hpp"
+
+TEST(PanicTest, CheckPanic) {
+    // Call CheckPanic with a true condition
+    CheckPanic(true, "This should not panic");
+
+    // Call CheckPanic with a false condition
+    EXPECT_DEATH(CheckPanic(false, "This should panic"), "");
+}

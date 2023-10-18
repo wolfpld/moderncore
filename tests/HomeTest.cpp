d5bf75cbc2de1460c60a1906e4afcf394e04f072
Bartosz Taudul <wolf@nereid.pl>
2023-10-18 23:10:35 +0200

    Add tests for Home.

---
+++ tests/HomeTest.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "../src/util/Home.hpp"
+
+TEST(HomeTest, GetHome) {
+    // Check that GetHome returns the correct home directory
+    EXPECT_EQ(GetHome(), getenv("HOME"));
+}
+
+TEST(HomeTest, ExpandHome) {
+    // Check that ExpandHome returns the correct expanded path
+    EXPECT_EQ(ExpandHome("~/test"), std::string(getenv("HOME")) + "/test");
+
+    // Check that ExpandHome returns the original path if it doesn't start with ~
+    EXPECT_EQ(ExpandHome("/test"), "/test");
+}

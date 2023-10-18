212f03d2f7973cb5b39b90705d369ab35997df05
Bartosz Taudul <wolf@nereid.pl>
2023-10-18 23:04:39 +0200

    Add test for FileWrapper.

---
+++ tests/FileWrapperTest.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+#include "../src/util/FileWrapper.hpp"
+
+TEST(FileWrapperTest, ReadFile) {
+    // Create a test file with some data
+    const char* filename = "test.bin";
+    FILE* file = fopen(filename, "wb");
+    fwrite("test data", 1, 9, file);
+    fclose(file);
+
+    // Open the test file with FileWrapper
+    FileWrapper fileWrapper(filename, "rb");
+
+    // Read the data from the file
+    char buffer[10] = {};
+    bool success = fileWrapper.Read(buffer, 9);
+
+    // Check that the read was successful and that the buffer contains the correct data
+    EXPECT_TRUE(success);
+    EXPECT_STREQ(buffer, "test data");
+
+    // Delete the test file
+    std::remove(filename);
+}

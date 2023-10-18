3b22fa45498a4d4a372f18d1b2df0568c15e09a1
Bartosz Taudul <wolf@nereid.pl>
2023-10-18 02:32:07 +0200

    Add test for FileBuffer.

---
+++ tests/FileBufferTest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "../src/util/FileBuffer.hpp"
+
+TEST(FileBufferTest, LoadFile) {
+    // Create a test file with some data
+    const char* filename = "test.bin";
+    FILE* file = fopen(filename, "wb");
+    fwrite("test data", 1, 9, file);
+    fclose(file);
+
+    // Load the test file into a file buffer
+    FileBuffer buffer(filename);
+
+    // Check that the buffer contains the correct data
+    EXPECT_EQ(buffer.size(), 9);
+    EXPECT_STREQ(buffer.data(), "test data");
+
+    // Delete the test file
+    std::remove(filename);
+}

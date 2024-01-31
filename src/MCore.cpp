e30c4ad25720c063a0f1285fa2252ce3c29228ab
Bartosz Taudul <wolf@nereid.pl>
2024-01-31 01:15:49 +0100

    Add silly startup prompt.

--- src/MCore.cpp
+++ src/MCore.cpp
@@ -1,7 +1,10 @@
 #include "server/Server.hpp"
+#include "util/Ansi.hpp"
 
 int main()
 {
+    printf( "Starting " ANSI_BOLD ANSI_ITALIC "Modern Core" ANSI_RESET "...\n\n" );
+
     Server server;
     server.Run();
     return 0;

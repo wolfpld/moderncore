bc91c1159c91f3aa8e7f59111bfd19fe2228ca8a
Bartosz Taudul <wolf@nereid.pl>
2024-02-02 00:19:10 +0100

    Parse parameters and use --debug to enable debug logging.

--- src/MCore.cpp
+++ src/MCore.cpp
@@ -1,9 +1,30 @@
+#include <getopt.h>
+
 #include "server/Server.hpp"
 #include "util/Ansi.hpp"
+#include "util/Logs.hpp"
 
-int main()
+int main( int argc, char** argv )
 {
     printf( "Starting " ANSI_BOLD ANSI_ITALIC "Modern Core" ANSI_RESET "...\n\n" );
+
+    struct option longOptions[] = {
+        { "debug", no_argument, nullptr, 'd' },
+        {}
+    };
+
+    int opt;
+    while( ( opt = getopt_long( argc, argv, "d", longOptions, nullptr ) ) != -1 )
+    {
+        switch (opt)
+        {
+        case 'd':
+            SetLogLevel( LogLevel::Debug );
+            break;
+        default:
+            break;
+        }
+    }
 
     Server server;
     server.Run();

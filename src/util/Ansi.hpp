e6fecaf745e7e9cc64e4dd92493d16055a25f88e
Bartosz Taudul <wolf@nereid.pl>
2024-02-01 20:52:35 +0100

    Add missing ANSI colors.

--- src/util/Ansi.hpp
+++ src/util/Ansi.hpp
@@ -10,6 +10,8 @@
 #define ANSI_RED "\033[31m"
 #define ANSI_GREEN "\033[32m"
 #define ANSI_YELLOW "\033[33m"
+#define ANSI_BLUE "\033[34m"
 #define ANSI_MAGENTA "\033[35m"
 #define ANSI_CYAN "\033[36m"
+#define ANSI_WHITE "\033[37m"
 #define ANSI_ERASE_LINE "\033[2K"

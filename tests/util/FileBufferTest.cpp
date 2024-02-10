3799e81419c05d521d38b0e81d52d75bea3ad497
Bartosz Taudul <wolf@nereid.pl>
2024-02-10 22:53:25 +0100

    Fix FileBufferTest.

--- tests/util/FileBufferTest.cpp
+++ tests/util/FileBufferTest.cpp
@@ -6,14 +6,14 @@ TEST(FileBufferTest, LoadFile) {
     // Create a test file with some data
     const char* filename = "test.bin";
     FILE* file = fopen(filename, "wb");
-    fwrite("test data", 1, 9, file);
+    fwrite("test data", 1, 10, file);
     fclose(file);
 
     // Load the test file into a file buffer
     FileBuffer buffer(filename);
 
     // Check that the buffer contains the correct data
-    EXPECT_EQ(buffer.size(), 9);
+    EXPECT_EQ(buffer.size(), 10);
     EXPECT_STREQ(buffer.data(), "test data");
 
     // Delete the test file

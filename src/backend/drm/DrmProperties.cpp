e81e7951c4c255738d0d3c05fcfbc3937063a252
Bartosz Taudul <wolf@nereid.pl>
2024-02-16 00:12:46 +0100

    Add a wrapper for DRM properties.

---
+++ src/backend/drm/DrmProperties.cpp
@@ -0,0 +1,55 @@
+#include <string.h>
+
+#include "DrmProperties.hpp"
+
+/* DrmProperty */
+
+DrmProperty::DrmProperty( int fd, drmModePropertyPtr prop, uint64_t value )
+    : m_fd( fd )
+    , m_value( value )
+    , m_prop( prop )
+    , m_blob( nullptr )
+{
+}
+
+DrmProperty::~DrmProperty()
+{
+    if( m_blob ) drmModeFreePropertyBlob( m_blob );
+    if( m_prop ) drmModeFreeProperty( m_prop );
+}
+
+drmModePropertyBlobPtr DrmProperty::Blob()
+{
+    if( m_blob ) return m_blob;
+    if( m_prop->flags & DRM_MODE_PROP_BLOB )
+    {
+        m_blob = drmModeGetPropertyBlob( m_fd, m_value );
+        return m_blob;
+    }
+    return nullptr;
+}
+
+/* DrmProperties */
+
+DrmProperties::DrmProperties( int fd, uint32_t id, uint32_t type )
+    : m_fd( fd )
+    , m_props( drmModeObjectGetProperties( fd, id, type ) )
+{
+}
+
+DrmProperties::~DrmProperties()
+{
+    drmModeFreeObjectProperties( m_props );
+}
+
+DrmProperty DrmProperties::operator[]( const char* name ) const
+{
+    for( int i=0; i<m_props->count_props; i++ )
+    {
+        auto prop = drmModeGetProperty( m_fd, m_props->props[i] );
+        if( !prop ) continue;
+        if( strcmp( prop->name, name ) == 0 ) return DrmProperty( m_fd, prop, m_props->prop_values[i] );
+        drmModeFreeProperty( prop );
+    }
+    return {};
+}
---
+++ src/backend/drm/DrmProperties.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <stdint.h>
+#include <xf86drmMode.h>
+
+#include "util/NoCopy.hpp"
+
+class DrmProperty
+{
+public:
+    DrmProperty() : DrmProperty( 0, nullptr, 0 ) {}
+    explicit DrmProperty( int fd, drmModePropertyPtr prop, uint64_t value );
+    ~DrmProperty();
+
+    NoCopy( DrmProperty );
+
+    drmModePropertyBlobPtr Blob();
+
+    operator bool() const { return m_fd != 0; }
+
+private:
+    int m_fd;
+    uint64_t m_value;
+    drmModePropertyPtr m_prop;
+    drmModePropertyBlobPtr m_blob;
+};
+
+
+class DrmProperties
+{
+public:
+    DrmProperties( int fd, uint32_t id, uint32_t type );
+    ~DrmProperties();
+
+    NoCopy( DrmProperties );
+
+    [[nodiscard]] DrmProperty operator[]( const char* name ) const;
+
+    operator bool() const { return m_props; }
+
+private:
+    int m_fd;
+    drmModeObjectPropertiesPtr m_props;
+};

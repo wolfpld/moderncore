e740e296c56e4365dd0f8e85ffc64204cf478341
Bartosz Taudul <wolf@nereid.pl>
2024-03-08 01:30:40 +0100

    Bump Vulkan API level to 1.3.

--- src/vulkan/VlkInstance.hpp
+++ src/vulkan/VlkInstance.hpp
@@ -27,7 +27,7 @@ public:
 
     [[nodiscard]] auto& QueryPhysicalDevices() const { return m_physicalDevices; }
     [[nodiscard]] auto Type() const { return m_instanceType; }
-    [[nodiscard]] auto ApiVersion() const { return VK_API_VERSION_1_2; }
+    [[nodiscard]] auto ApiVersion() const { return VK_API_VERSION_1_3; }
 
     operator VkInstance() const { return m_instance; }
 

dc040d0a0d0230185a21a5ace188eb7a224ad8fb
Bartosz Taudul <wolf@nereid.pl>
2024-03-10 00:45:03 +0100

    Add hacky macro to create on-heap Tracy Vulkan zone.

--- src/util/Tracy.hpp
+++ src/util/Tracy.hpp
@@ -10,4 +10,6 @@
 
 #define ZoneVkDevice( device ) { auto& properties = device->Properties(); ZoneText( properties.deviceName, strlen( properties.deviceName ) ); }
 
+#define ZoneVkNew( ctx, varname, cmdbuf, name, active ) { static constexpr tracy::SourceLocationData TracyConcat(__tracy_gpu_source_location,TracyLine) { name, TracyFunction, TracyFile, (uint32_t)TracyLine, 0 }; varname = new tracy::VkCtxScope( ctx, &TracyConcat(__tracy_gpu_source_location,TracyLine), cmdbuf, active ); }
+
 #endif

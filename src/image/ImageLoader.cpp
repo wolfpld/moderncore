3a54054aea91a926ff12a01d0a3ca8757cdb9004
Bartosz Taudul <wolf@nereid.pl>
2024-11-09 22:03:22 +0100

    Add TIFF image loading support.

--- src/image/ImageLoader.cpp
+++ src/image/ImageLoader.cpp
@@ -9,6 +9,7 @@
 #include "PngLoader.hpp"
 #include "PvrLoader.hpp"
 #include "StbImageLoader.hpp"
+#include "TiffLoader.hpp"
 #include "WebpLoader.hpp"
 #include "util/Bitmap.hpp"
 #include "util/FileWrapper.hpp"
@@ -53,6 +54,7 @@ Bitmap* LoadImage( const char* filename )
     if( auto img = LoadImage<PvrLoader>( file ); img ) return img;
     if( auto img = LoadImage<DdsLoader>( file ); img ) return img;
     if( auto img = LoadImage<StbImageLoader>( file ); img ) return img;
+    if( auto img = LoadImage<TiffLoader>( file ); img ) return img;
 
     mclog( LogLevel::Error, "Failed to load image %s", path.c_str() );
     return nullptr;

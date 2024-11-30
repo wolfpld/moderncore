a8b22a171ff94d82e9ec06891716e99197b0b3f5
Bartosz Taudul <wolf@nereid.pl>
2024-11-30 16:31:36 +0100

    Add check for animtion to WebP loader.

--- src/image/WebpLoader.cpp
+++ src/image/WebpLoader.cpp
@@ -8,10 +8,16 @@
 
 WebpLoader::WebpLoader( std::shared_ptr<FileWrapper> file )
     : ImageLoader( std::move( file ) )
+    , m_dec( nullptr )
 {
     fseek( *m_file, 0, SEEK_SET );
     uint8_t hdr[12];
     m_valid = fread( hdr, 1, 12, *m_file ) == 12 && memcmp( hdr, "RIFF", 4 ) == 0 && memcmp( hdr + 8, "WEBP", 4 ) == 0;
+}
+
+WebpLoader::~WebpLoader()
+{
+    if( m_dec ) WebPAnimDecoderDelete( m_dec );
 }
 
 bool WebpLoader::IsValid() const
@@ -19,15 +25,43 @@ bool WebpLoader::IsValid() const
     return m_valid;
 }
 
+bool WebpLoader::IsAnimated()
+{
+    if( !m_dec ) Open();
+
+    WebPAnimInfo info;
+    WebPAnimDecoderGetInfo( m_dec, &info );
+
+    return info.frame_count > 1;
+}
+
 std::unique_ptr<Bitmap> WebpLoader::Load()
 {
-    CheckPanic( m_valid, "Invalid WebP file" );
+    if( !m_dec ) Open();
 
-    FileBuffer buf( m_file );
+    WebPAnimInfo info;
+    WebPAnimDecoderGetInfo( m_dec, &info );
+
+    int delay;
+    uint8_t* out;
+    if( !WebPAnimDecoderGetNext( m_dec, &out, &delay ) ) return nullptr;
+
+    auto bmp = std::make_unique<Bitmap>( info.canvas_width, info.canvas_height );
+    memcpy( bmp->Data(), out, info.canvas_width * info.canvas_height * 4 );
+
+    return bmp;
+}
+
+void WebpLoader::Open()
+{
+    CheckPanic( m_valid, "Invalid WebP file" );
+    CheckPanic( !m_buf && !m_dec, "Already opened" );
+
+    m_buf = std::make_unique<FileBuffer>( m_file );
 
     WebPData data = {
-        .bytes = (const uint8_t*)buf.data(),
-        .size = buf.size()
+        .bytes = (const uint8_t*)m_buf->data(),
+        .size = m_buf->size()
     };
 
     WebPAnimDecoderOptions opts;
@@ -35,22 +69,6 @@ std::unique_ptr<Bitmap> WebpLoader::Load()
     opts.color_mode = MODE_RGBA;
     opts.use_threads = 1;
 
-    WebPAnimDecoder* dec = WebPAnimDecoderNew( &data, &opts );
-
-    WebPAnimInfo info;
-    WebPAnimDecoderGetInfo( dec, &info );
-
-    int delay;
-    uint8_t* out;
-    if( !WebPAnimDecoderGetNext( dec, &out, &delay ) )
-    {
-        WebPAnimDecoderDelete( dec );
-        return nullptr;
-    }
-
-    auto bmp = std::make_unique<Bitmap>( info.canvas_width, info.canvas_height );
-    memcpy( bmp->Data(), out, info.canvas_width * info.canvas_height * 4 );
-
-    WebPAnimDecoderDelete( dec );
-    return bmp;
+    m_dec = WebPAnimDecoderNew( &data, &opts );
+    CheckPanic( m_dec, "Failed to open WebP file" );
 }
--- src/image/WebpLoader.hpp
+++ src/image/WebpLoader.hpp
@@ -1,21 +1,35 @@
 #pragma once
 
+#include <memory>
+#include <stdint.h>
+
 #include "ImageLoader.hpp"
 #include "util/FileWrapper.hpp"
 #include "util/NoCopy.hpp"
 
 class Bitmap;
+class FileBuffer;
+
+typedef struct WebPAnimDecoder WebPAnimDecoder;
 
 class WebpLoader : public ImageLoader
 {
 public:
     explicit WebpLoader( std::shared_ptr<FileWrapper> file );
+    ~WebpLoader() override;
 
     NoCopy( WebpLoader );
 
     [[nodiscard]] bool IsValid() const override;
+    [[nodiscard]] bool IsAnimated() override;
+
     [[nodiscard]] std::unique_ptr<Bitmap> Load() override;
 
 private:
+    void Open();
+
     bool m_valid;
+
+    std::unique_ptr<FileBuffer> m_buf;
+    WebPAnimDecoder* m_dec;
 };

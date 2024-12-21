5ec6db082a56c0984bbe121623974113c370d202
Bartosz Taudul <wolf@nereid.pl>
2024-12-21 13:41:53 +0100

    AVX2 linear to sRGB conversion.

--- src/util/Tonemapper.cpp
+++ src/util/Tonemapper.cpp
@@ -55,7 +55,37 @@ float LinearToSrgb( float x )
 void PbrNeutral( uint32_t* dst, float* src, size_t sz )
 {
 #if defined __SSE4_1__ && defined __FMA__
-    do
+#if defined __AVX2__
+    while( sz > 1 )
+    {
+        const HdrColor color[2] = {
+            PbrNeutral( { src[0], src[1], src[2] } ),
+            PbrNeutral( { src[4], src[5], src[6] } )
+        };
+
+        __m256 s0 = _mm256_loadu_ps( src );
+        __m256 v0 = _mm256_loadu_ps( (const float*)color );
+        __m256 v1 = _mm256_cmp_ps( v0, _mm256_set1_ps( 0.0031308f ), _CMP_LE_OQ );
+        __m256 v2 = _mm256_mul_ps( v0, _mm256_set1_ps( 12.92f ) );
+        __m256 v3 = _mm256_pow_ps( v0, _mm256_set1_ps( 1.0f / 2.4f ) );
+        __m256 v4 = _mm256_mul_ps( v3, _mm256_set1_ps( 1.055f ) );
+        __m256 v5 = _mm256_sub_ps( v4, _mm256_set1_ps( 0.055f ) );
+        __m256 v6 = _mm256_blendv_ps( v5, v2, v1 );
+        __m256 v7 = _mm256_blend_ps( v6, s0, 0x88 );
+        __m256 v8 = _mm256_min_ps( v7, _mm256_set1_ps( 1.0f ) );
+        __m256 v9 = _mm256_max_ps( v8, _mm256_set1_ps( 0.0f ) );
+        __m256 v10 = _mm256_mul_ps( v9, _mm256_set1_ps( 255.0f ) );
+        __m256i v11 = _mm256_cvtps_epi32( v10 );
+        __m256i v12 = _mm256_packus_epi32( v11, v11 );
+        __m256i v13 = _mm256_packus_epi16( v12, v12 );
+        *dst++ = _mm_cvtsi128_si32( _mm256_castsi256_si128( v13 ) );
+        *dst++ = _mm_cvtsi128_si32( _mm256_extracti128_si256( v13, 1 ) );
+
+        src += 8;
+        sz -= 2;
+    }
+#endif
+    while( sz > 0 )
     {
         const auto color = PbrNeutral( { src[0], src[1], src[2] } );
 
@@ -77,8 +107,8 @@ void PbrNeutral( uint32_t* dst, float* src, size_t sz )
         *dst++ = _mm_cvtsi128_si32( v13 );
 
         src += 4;
+        sz--;
     }
-    while( --sz );
 #else
     do
     {

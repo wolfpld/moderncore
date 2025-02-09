32208b1e749948a013b57d88240c5ba74fa22842
Bartosz Taudul <wolf@nereid.pl>
2025-02-09 19:16:41 +0100

    Allow functions registered with Method() to return a value.
    In this context return void is legal, as the return type has to be deduced.

--- src/util/Invoke.hpp
+++ src/util/Invoke.hpp
@@ -2,6 +2,6 @@
 
 #include "util/Panic.hpp"
 
-#define Method( func ) [](void* ptr, auto... args) { CheckPanic( ptr, "Class pointer is nullptr" ); ((decltype(this))ptr)->func( args... ); }
+#define Method( func ) [](void* ptr, auto... args) { CheckPanic( ptr, "Class pointer is nullptr" ); return ((decltype(this))ptr)->func( args... ); }
 
 #define Invoke( func, ... ) if( m_listener->func ) { m_listener->func( m_listenerPtr, ##__VA_ARGS__ ); }

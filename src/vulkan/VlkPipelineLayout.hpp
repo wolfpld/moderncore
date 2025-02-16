87c1b42067e4bb1dd58ecb08e72a3aa7f9375af9
Bartosz Taudul <wolf@nereid.pl>
2025-02-16 21:48:27 +0100

    Make VlkPipelineLayout inherit from VlkBase.

--- src/vulkan/VlkPipelineLayout.hpp
+++ src/vulkan/VlkPipelineLayout.hpp
@@ -2,9 +2,10 @@
 
 #include <vulkan/vulkan.h>
 
+#include "VlkBase.hpp"
 #include "util/NoCopy.hpp"
 
-class VlkPipelineLayout
+class VlkPipelineLayout : public VlkBase
 {
 public:
     VlkPipelineLayout( VkDevice device, const VkPipelineLayoutCreateInfo& createInfo );

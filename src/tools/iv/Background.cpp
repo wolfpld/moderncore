77945c5ba908d209e18fcf27426a3627ac05f910
Bartosz Taudul <wolf@nereid.pl>
2025-02-16 21:48:44 +0100

    Create Background object rendering pipeline.

--- src/tools/iv/Background.cpp
+++ src/tools/iv/Background.cpp
@@ -7,6 +7,8 @@
 #include "util/EmbedData.hpp"
 #include "vulkan/VlkBuffer.hpp"
 #include "vulkan/VlkDevice.hpp"
+#include "vulkan/VlkPipeline.hpp"
+#include "vulkan/VlkPipelineLayout.hpp"
 #include "vulkan/VlkShader.hpp"
 #include "vulkan/VlkShaderModule.hpp"
 #include "vulkan/ext/GarbageChute.hpp"
@@ -29,6 +31,88 @@ Background::Background( GarbageChute& garbage, std::shared_ptr<VlkDevice> device
     };
     m_shader = std::make_shared<VlkShader>( stages );
 
+
+    constexpr VkPipelineLayoutCreateInfo pipelineLayoutInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO
+    };
+    m_pipelineLayout = std::make_shared<VlkPipelineLayout>( *m_device, pipelineLayoutInfo );
+
+
+    static constexpr VkVertexInputBindingDescription vertexBindingDescription = {
+        .binding = 0,
+        .stride = sizeof( Vertex ),
+        .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
+    };
+    static constexpr std::array vertexAttributeDescription = {
+        VkVertexInputAttributeDescription { 0, 0, VK_FORMAT_R32G32_SFLOAT, offsetof( Vertex, x ) }
+    };
+    constexpr VkPipelineVertexInputStateCreateInfo vertexInputInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        .vertexBindingDescriptionCount = 1,
+        .pVertexBindingDescriptions = &vertexBindingDescription,
+        .vertexAttributeDescriptionCount = vertexAttributeDescription.size(),
+        .pVertexAttributeDescriptions = vertexAttributeDescription.data()
+    };
+    constexpr VkPipelineInputAssemblyStateCreateInfo inputAssembly = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+        .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST
+    };
+    constexpr VkPipelineViewportStateCreateInfo viewportState = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+        .viewportCount = 1,
+        .scissorCount = 1
+    };
+    constexpr VkPipelineRasterizationStateCreateInfo rasterizer = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        .polygonMode = VK_POLYGON_MODE_FILL,
+        .lineWidth = 1.0f
+    };
+    constexpr VkPipelineMultisampleStateCreateInfo multisampling = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT
+    };
+    static constexpr VkPipelineColorBlendAttachmentState colorBlendAttachment = {
+        .colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT
+    };
+    constexpr VkPipelineColorBlendStateCreateInfo colorBlending = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+        .attachmentCount = 1,
+        .pAttachments = &colorBlendAttachment
+    };
+    static constexpr std::array colorAttachmentFormats = {
+        VK_FORMAT_R8G8B8A8_SRGB
+    };
+    constexpr VkPipelineRenderingCreateInfo rendering = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO,
+        .colorAttachmentCount = colorAttachmentFormats.size(),
+        .pColorAttachmentFormats = colorAttachmentFormats.data()
+    };
+    static constexpr std::array dynamicStateList = {
+        VK_DYNAMIC_STATE_VIEWPORT,
+        VK_DYNAMIC_STATE_SCISSOR
+    };
+    constexpr VkPipelineDynamicStateCreateInfo dynamicState = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
+        .dynamicStateCount = dynamicStateList.size(),
+        .pDynamicStates = dynamicStateList.data()
+    };
+    const VkGraphicsPipelineCreateInfo pipelineInfo = {
+        .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
+        .pNext = &rendering,
+        .stageCount = m_shader->GetStageCount(),
+        .pStages = m_shader->GetStages(),
+        .pVertexInputState = &vertexInputInfo,
+        .pInputAssemblyState = &inputAssembly,
+        .pViewportState = &viewportState,
+        .pRasterizationState = &rasterizer,
+        .pMultisampleState = &multisampling,
+        .pColorBlendState = &colorBlending,
+        .pDynamicState = &dynamicState,
+        .layout = *m_pipelineLayout,
+    };
+    m_pipeline = std::make_shared<VlkPipeline>( *m_device, pipelineInfo );
+
+
     constexpr Vertex vdata[] = {
         { -1, -1 },
         { -1,  3 },
@@ -47,6 +131,8 @@ Background::Background( GarbageChute& garbage, std::shared_ptr<VlkDevice> device
 
 Background::~Background()
 {
+    m_garbage.Recycle( std::move( m_pipeline ) );
+    m_garbage.Recycle( std::move( m_pipelineLayout ) );
     m_garbage.Recycle( std::move( m_shader ) );
     m_garbage.Recycle( std::move( m_vertexBuffer ) );
 }
--- src/tools/iv/Background.hpp
+++ src/tools/iv/Background.hpp
@@ -6,6 +6,7 @@ class GarbageChute;
 class VlkBuffer;
 class VlkDevice;
 class VlkPipeline;
+class VlkPipelineLayout;
 class VlkShader;
 
 class Background
@@ -19,6 +20,7 @@ private:
     std::shared_ptr<VlkDevice> m_device;
 
     std::shared_ptr<VlkShader> m_shader;
+    std::shared_ptr<VlkPipelineLayout> m_pipelineLayout;
     std::shared_ptr<VlkPipeline> m_pipeline;
     std::shared_ptr<VlkBuffer> m_vertexBuffer;
 };

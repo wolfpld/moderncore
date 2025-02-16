aa2084359a7247258f1f8bc78ac028b37d258d35
Bartosz Taudul <wolf@nereid.pl>
2025-02-16 17:31:58 +0100

    Add a garbage chute interface for throwing away Vulkan objects.

---
+++ src/vulkan/ext/GarbageChute.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+class VlkBase;
+
+class GarbageChute
+{
+public:
+    ~GarbageChute() = default;
+
+    virtual void Recycle( std::shared_ptr<VlkBase>&& garbage ) = 0;
+    virtual void Recycle( std::vector<std::shared_ptr<VlkBase>>&& garbage ) = 0;
+};

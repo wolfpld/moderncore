6a14283f99f9208bd2055b78aa5fafcf352cb3e7
Bartosz Taudul <wolf@nereid.pl>
2025-02-16 19:01:10 +0100

    Load shader in Background class.

--- src/tools/iv/Background.cpp
+++ src/tools/iv/Background.cpp
@@ -1,2 +1,30 @@
-#include "Background.hpp"
+#include <array>
 
+#include "Background.hpp"
+#include "BackgroundFrag.hpp"
+#include "BackgroundVert.hpp"
+#include "util/EmbedData.hpp"
+#include "vulkan/VlkDevice.hpp"
+#include "vulkan/VlkShader.hpp"
+#include "vulkan/VlkShaderModule.hpp"
+#include "vulkan/ext/GarbageChute.hpp"
+
+Background::Background( GarbageChute& garbage, std::shared_ptr<VlkDevice> device )
+    : m_garbage( garbage )
+    , m_device( std::move( device ) )
+{
+    Unembed( BackgroundVert );
+    Unembed( BackgroundFrag );
+
+    std::array stages = {
+        VlkShader::Stage { std::make_shared<VlkShaderModule>( *m_device, BackgroundVert ), VK_SHADER_STAGE_VERTEX_BIT },
+        VlkShader::Stage { std::make_shared<VlkShaderModule>( *m_device, BackgroundFrag ), VK_SHADER_STAGE_FRAGMENT_BIT }
+    };
+
+    m_shader = std::make_shared<VlkShader>( stages );
+}
+
+Background::~Background()
+{
+    m_garbage.Recycle( std::move( m_shader ) );
+}
--- src/tools/iv/Background.hpp
+++ src/tools/iv/Background.hpp
@@ -2,17 +2,22 @@
 
 #include <memory>
 
+class GarbageChute;
 class VlkBuffer;
+class VlkDevice;
 class VlkPipeline;
 class VlkShader;
 
 class Background
 {
 public:
-    Background();
+    Background( GarbageChute& garbage, std::shared_ptr<VlkDevice> device );
     ~Background();
 
 private:
+    GarbageChute& m_garbage;
+    std::shared_ptr<VlkDevice> m_device;
+
     std::shared_ptr<VlkShader> m_shader;
     std::shared_ptr<VlkPipeline> m_pipeline;
     std::shared_ptr<VlkBuffer> m_vertexBuffer;

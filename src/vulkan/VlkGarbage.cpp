9a287a2725ff80e448dc14f94291c5df3c4ffdef
Bartosz Taudul <wolf@nereid.pl>
2025-02-17 22:31:06 +0100

    Improve VlkGarbage logs.

--- src/vulkan/VlkGarbage.cpp
+++ src/vulkan/VlkGarbage.cpp
@@ -6,7 +6,7 @@
 VlkGarbage::~VlkGarbage()
 {
     if( m_garbage.empty() ) return;
-    mclog( LogLevel::Debug, "Waiting for %zu garbage items", m_garbage.size() );
+    mclog( LogLevel::Debug, "Waiting for %zu garbage sets", m_garbage.size() );
     for( auto& g : m_garbage ) g.first->Wait();
 }
 
@@ -39,14 +39,16 @@ void VlkGarbage::Recycle( std::shared_ptr<VlkFence> fence, std::vector<std::shar
 void VlkGarbage::Collect()
 {
     size_t count = 0;
+    size_t sets = 0;
 
     auto it = m_garbage.begin();
     while( it != m_garbage.end() )
     {
         if( it->first->Wait( 0 ) == VK_SUCCESS )
         {
+            count += it->second.size();
+            sets++;
             it = m_garbage.erase( it );
-            ++count;
         }
         else
         {
@@ -54,5 +56,5 @@ void VlkGarbage::Collect()
         }
     }
 
-    if( count > 0 ) mclog( LogLevel::Debug, "Garbage collected: %zu", count );
+    if( count > 0 ) mclog( LogLevel::Debug, "Garbage collected: %zu sets, %zu items", sets, count );
 }

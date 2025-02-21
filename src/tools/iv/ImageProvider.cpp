ef019685e1442708cabf5d963d9aad33e5409335
Bartosz Taudul <wolf@nereid.pl>
2025-02-21 20:15:15 +0100

    Implement image loading logic.

--- src/tools/iv/ImageProvider.cpp
+++ src/tools/iv/ImageProvider.cpp
@@ -1,9 +1,105 @@
 #include "ImageProvider.hpp"
+#include "image/ImageLoader.hpp"
+#include "util/Bitmap.hpp"
+#include "util/BitmapHdr.hpp"
+#include "util/Logs.hpp"
 
 ImageProvider::ImageProvider()
+    : m_td( std::max( 1u, std::thread::hardware_concurrency() - 1 ), "Image Provider" )
 {
 }
 
 ImageProvider::~ImageProvider()
 {
+    if( m_thread.joinable() ) m_thread.join();
+}
+
+void ImageProvider::LoadImage( const char* path, Callback callback, void* userData )
+{
+    {
+        std::lock_guard lock( m_lock );
+
+        if( m_job )
+        {
+            m_job->cancel = true;
+            m_job.reset();
+            m_thread.detach();
+        }
+        else if( m_thread.joinable() )
+        {
+            m_thread.join();
+        }
+    }
+
+    m_job = std::make_shared<Job>( Job {
+        .path = path,
+        .callback = callback,
+        .userData = userData,
+        .cancel = false
+    } );
+
+    m_thread = std::thread( [this] { Worker( m_job ); } );
+}
+
+// Worker signals that the job is done by reseting m_job
+void ImageProvider::Worker( std::shared_ptr<Job> job )
+{
+    mclog( LogLevel::Info, "Loading image %s", job->path.c_str() );
+    auto loader = GetImageLoader( job->path.c_str(), ToneMap::Operator::PbrNeutral, &m_td );
+    if( !loader )
+    {
+        std::lock_guard lock( m_lock );
+        if( !job->cancel ) m_job.reset();
+        job->callback( job->userData, job->cancel ? Cancelled : Error, {} );
+        return;
+    }
+
+    std::unique_ptr<Bitmap> bitmap;
+    if( loader->IsHdr() && loader->PreferHdr() )
+    {
+        auto hdr = loader->LoadHdr();
+        bitmap = std::make_unique<Bitmap>( hdr->Width(), hdr->Height() );
+
+        {
+            std::lock_guard lock( m_lock );
+            if( job->cancel )
+            {
+                job->callback( job->userData, Cancelled, {} );
+                return;
+            }
+        }
+
+        auto src = hdr->Data();
+        auto dst = bitmap->Data();
+        size_t sz = hdr->Width() * hdr->Height();
+        while( sz > 0 )
+        {
+            const auto chunk = std::min( sz, size_t( 16 * 1024 ) );
+            m_td.Queue( [src, dst, chunk] {
+                ToneMap::Process( ToneMap::Operator::PbrNeutral, (uint32_t*)dst, src, chunk );
+            } );
+            src += chunk * 4;
+            dst += chunk * 4;
+            sz -= chunk;
+        }
+        m_td.Sync();
+    }
+    else
+    {
+        bitmap = loader->Load();
+    }
+
+    // TODO do this with geometry data
+    bitmap->NormalizeOrientation();
+
+    std::lock_guard lock( m_lock );
+    if( job->cancel )
+    {
+        job->callback( job->userData, Cancelled, {} );
+    }
+    else
+    {
+        m_job.reset();
+        job->callback( job->userData, Success, std::move( bitmap ) );
+    }
 }
--- src/tools/iv/ImageProvider.hpp
+++ src/tools/iv/ImageProvider.hpp
@@ -1,10 +1,45 @@
 #pragma once
 
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include "util/TaskDispatch.hpp"
+
+class Bitmap;
+
 class ImageProvider
 {
 public:
+    enum Result
+    {
+        Success,
+        Error,
+        Cancelled
+    };
+
+    typedef void(*Callback)( void* userData, int result, std::shared_ptr<Bitmap> bitmap );
+
     ImageProvider();
     ~ImageProvider();
 
+    void LoadImage( const char* path, Callback callback, void* userData );
+
 private:
+    struct Job
+    {
+        std::string path;
+        Callback callback;
+        void* userData;
+        bool cancel;
+    };
+
+    void Worker( std::shared_ptr<Job> job );
+
+    TaskDispatch m_td;
+
+    std::mutex m_lock;
+    std::thread m_thread;
+    std::shared_ptr<Job> m_job;
 };

eb55dc97b132d6fe0306508327e2a7af9da45656
Bartosz Taudul <wolf@nereid.pl>
2025-02-24 22:04:53 +0100

    Add wayland cursor enumeration.

---
+++ src/wayland/WaylandCursor.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+// Maps wp_cursor_shape_device_v1_shape
+enum class WaylandCursor
+{
+    None,
+    Default,
+    ContextMenu,
+    Help,
+    Pointer,
+    Progress,
+    Wait,
+    Cell,
+    Crosshair,
+    Text,
+    VerticalText,
+    Alias,
+    Copy,
+    Move,
+    NoDrop,
+    NotAllowed,
+    Grab,
+    Grabbing,
+    ResizeE,
+    ResizeN,
+    ResizeNE,
+    ResizeNW,
+    ResizeS,
+    ResizeSE,
+    ResizeSW,
+    ResizeW,
+    ResizeEW,
+    ResizeNS,
+    ResizeNESW,
+    ResizeNWSE,
+    ResizeCol,
+    ResizeRow,
+    AllScroll,
+    ZoomIn,
+    ZoomOut
+};

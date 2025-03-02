01ddbeb47a538fbdd404a442a6d72cc6a384c810
Bartosz Taudul <wolf@nereid.pl>
2025-03-02 02:44:08 +0100

    Add UrlDecode function.

---
+++ src/util/Url.cpp
@@ -0,0 +1,17 @@
+#include "Url.hpp"
+
+void UrlDecode( std::string& url )
+{
+    size_t pos = 0;
+    while( ( pos = url.find( "%", pos ) ) != std::string::npos )
+    {
+        if( pos + 2 >= url.size() )
+        {
+            url.erase( pos );
+            break;
+        }
+        const auto c = std::stoi( url.substr( pos + 1, 2 ), nullptr, 16 );
+        url.replace( pos, 3, 1, c );
+        pos += 1;
+    }
+}

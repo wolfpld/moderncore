c46231c79a7e35d05ced9385be32e2ca2ca05e41
Bartosz Taudul <wolf@nereid.pl>
2025-03-09 18:52:12 +0100

    Modernize struct init in VlkShaderModule.

--- src/vulkan/VlkShaderModule.cpp
+++ src/vulkan/VlkShaderModule.cpp
@@ -4,10 +4,11 @@
 VlkShaderModule::VlkShaderModule( VkDevice device, const char* data, size_t size )
     : m_device( device )
 {
-    VkShaderModuleCreateInfo info = { VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO };
-    info.codeSize = size;
-    info.pCode = (const uint32_t*)data;
-
+    const VkShaderModuleCreateInfo info = {
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .codeSize = size,
+        .pCode = (const uint32_t*)data
+    };
     VkVerify( vkCreateShaderModule( device, &info, nullptr, &m_module ) );
 }
 

456eca95d62db55e114dc75c3d4d030f86ac57aa
Bartosz Taudul <wolf@nereid.pl>
2025-06-14 21:15:52 +0200

    Extract opening jpeg header.

--- src/image/JpgLoader.cpp
+++ src/image/JpgLoader.cpp
@@ -17,10 +17,18 @@
 
 JpgLoader::JpgLoader( std::shared_ptr<FileWrapper> file )
     : m_file( std::move( file ) )
+    , m_cinfo( nullptr )
+    , m_iccData( nullptr )
 {
     fseek( *m_file, 0, SEEK_SET );
     uint8_t hdr[2];
     m_valid = fread( hdr, 1, 2, *m_file ) == 2 && hdr[0] == 0xFF && hdr[1] == 0xD8;
+}
+
+JpgLoader::~JpgLoader()
+{
+    free( m_iccData );
+    if( m_cinfo ) jpeg_destroy_decompress( m_cinfo );
 }
 
 bool JpgLoader::IsValidSignature( const uint8_t* buf, size_t size )
@@ -73,57 +81,42 @@ bool HasColorspaceExtensions()
 
 std::unique_ptr<Bitmap> JpgLoader::Load()
 {
-    CheckPanic( m_valid, "Invalid JPEG file" );
-    fseek( *m_file, 0, SEEK_SET );
-
-    const auto orientation = LoadOrientation();
-    JOCTET* icc = nullptr;
+    if( !m_cinfo && !Open() ) return nullptr;
 
     const bool extensions = HasColorspaceExtensions();
 
-    jpeg_decompress_struct cinfo;
     JpgErrorMgr jerr;
-
-    cinfo.err = jpeg_std_error( &jerr.pub );
+    m_cinfo->err = jpeg_std_error( &jerr.pub );
     jerr.pub.error_exit = []( j_common_ptr cinfo ) { longjmp( ((JpgErrorMgr*)cinfo->err)->setjmp_buffer, 1 ); };
-    if( setjmp( jerr.setjmp_buffer ) )
-    {
-        free( icc );
-        jpeg_destroy_decompress( &cinfo );
-        return nullptr;
-    }
+    if( setjmp( jerr.setjmp_buffer ) ) return nullptr;
 
-    jpeg_create_decompress( &cinfo );
-    jpeg_stdio_src( &cinfo, *m_file );
-    jpeg_save_markers( &cinfo, JPEG_APP0 + 2, 0xFFFF );
-    jpeg_read_header( &cinfo, TRUE );
-    const bool cmyk = cinfo.jpeg_color_space == JCS_CMYK || cinfo.jpeg_color_space == JCS_YCCK;
+    const bool cmyk = m_cinfo->jpeg_color_space == JCS_CMYK || m_cinfo->jpeg_color_space == JCS_YCCK;
 #ifdef JCS_EXTENSIONS
-    if( extensions && !cmyk ) cinfo.out_color_space = JCS_EXT_RGBX;
+    if( extensions && !cmyk ) m_cinfo->out_color_space = JCS_EXT_RGBX;
 #endif
-    jpeg_start_decompress( &cinfo );
+    jpeg_start_decompress( m_cinfo );
 
-    auto bmp = std::make_unique<Bitmap>( cinfo.output_width, cinfo.output_height, orientation );
+    auto bmp = std::make_unique<Bitmap>( m_cinfo->output_width, m_cinfo->output_height, m_orientation );
     auto ptr = bmp->Data();
 
     unsigned int iccSz;
-    jpeg_read_icc_profile( &cinfo, &icc, &iccSz );
+    jpeg_read_icc_profile( m_cinfo, &m_iccData, &iccSz );
 
     if( cmyk || extensions )
     {
-        while( cinfo.output_scanline < cinfo.output_height )
+        while( m_cinfo->output_scanline < m_cinfo->output_height )
         {
-            jpeg_read_scanlines( &cinfo, &ptr, 1 );
-            ptr += cinfo.output_width * 4;
+            jpeg_read_scanlines( m_cinfo, &ptr, 1 );
+            ptr += m_cinfo->output_width * 4;
         }
     }
     else
     {
-        auto row = new uint8_t[cinfo.output_width * 3 + 1];
-        while( cinfo.output_scanline < cinfo.output_height )
+        auto row = new uint8_t[m_cinfo->output_width * 3 + 1];
+        while( m_cinfo->output_scanline < m_cinfo->output_height )
         {
-            jpeg_read_scanlines( &cinfo, &row, 1 );
-            for( int i=0; i<cinfo.output_width; i++ )
+            jpeg_read_scanlines( m_cinfo, &row, 1 );
+            for( int i=0; i<m_cinfo->output_width; i++ )
             {
                 uint32_t col;
                 memcpy( &col, row + i * 3, 4 );
@@ -139,10 +132,10 @@ std::unique_ptr<Bitmap> JpgLoader::Load()
     {
         cmsHPROFILE profileIn;
 
-        if( icc )
+        if( m_iccData )
         {
             mclog( LogLevel::Info, "ICC profile size: %u", iccSz );
-            profileIn = cmsOpenProfileFromMem( icc, iccSz );
+            profileIn = cmsOpenProfileFromMem( m_iccData, iccSz );
         }
         else
         {
@@ -163,11 +156,11 @@ std::unique_ptr<Bitmap> JpgLoader::Load()
         cmsCloseProfile( profileOut );
         cmsCloseProfile( profileIn );
     }
-    else if( icc )
+    else if( m_iccData )
     {
         mclog( LogLevel::Info, "ICC profile size: %u", iccSz );
 
-        auto profileIn = cmsOpenProfileFromMem( icc, iccSz );
+        auto profileIn = cmsOpenProfileFromMem( m_iccData, iccSz );
         auto profileOut = cmsCreate_sRGBProfile();
         auto transform = cmsCreateTransform( profileIn, TYPE_RGBA_8, profileOut, TYPE_RGBA_8, INTENT_PERCEPTUAL, 0 );
 
@@ -181,12 +174,33 @@ std::unique_ptr<Bitmap> JpgLoader::Load()
         cmsCloseProfile( profileIn );
     }
 
-    free( icc );
-    jpeg_finish_decompress( &cinfo );
-    jpeg_destroy_decompress( &cinfo );
+    jpeg_finish_decompress( m_cinfo );
 
     bmp->SetAlpha( 0xFF );
     return bmp;
+}
+
+bool JpgLoader::Open()
+{
+    CheckPanic( m_valid, "Invalid JPEG file" );
+    CheckPanic( !m_cinfo, "Already opened" );
+
+    fseek( *m_file, 0, SEEK_SET );
+    m_orientation = LoadOrientation();
+
+    m_cinfo = new jpeg_decompress_struct();
+
+    JpgErrorMgr jerr;
+    m_cinfo->err = jpeg_std_error( &jerr.pub );
+    jerr.pub.error_exit = []( j_common_ptr cinfo ) { longjmp( ((JpgErrorMgr*)cinfo->err)->setjmp_buffer, 1 ); };
+    if( setjmp( jerr.setjmp_buffer ) ) return false;
+
+    jpeg_create_decompress( m_cinfo );
+    jpeg_stdio_src( m_cinfo, *m_file );
+    jpeg_save_markers( m_cinfo, JPEG_APP0 + 2, 0xFFFF );
+    jpeg_read_header( m_cinfo, TRUE );
+
+    return true;
 }
 
 int JpgLoader::LoadOrientation()
--- src/image/JpgLoader.hpp
+++ src/image/JpgLoader.hpp
@@ -8,11 +8,13 @@
 
 class Bitmap;
 class FileWrapper;
+struct jpeg_decompress_struct;
 
 class JpgLoader : public ImageLoader
 {
 public:
     explicit JpgLoader( std::shared_ptr<FileWrapper> file );
+    ~JpgLoader() override;
     NoCopy( JpgLoader );
 
     static bool IsValidSignature( const uint8_t* buf, size_t size );
@@ -21,8 +23,14 @@ public:
     [[nodiscard]] std::unique_ptr<Bitmap> Load() override;
 
 private:
+    [[nodiscard]] bool Open();
+
     int LoadOrientation();
 
     bool m_valid;
     std::shared_ptr<FileWrapper> m_file;
+
+    jpeg_decompress_struct* m_cinfo;
+    uint8_t* m_iccData;
+    int m_orientation;
 };

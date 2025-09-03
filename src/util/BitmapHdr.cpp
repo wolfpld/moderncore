98e830669c5710ba4dfb2e35bcbaabc79dc03027
Bartosz Taudul <wolf@nereid.pl>
2025-09-03 18:45:57 +0200

    Port orientation normalization from Bitmap to BitmapHdr.

--- src/util/BitmapHdr.cpp
+++ src/util/BitmapHdr.cpp
@@ -7,11 +7,12 @@
 #include "Panic.hpp"
 #include "TaskDispatch.hpp"
 
-BitmapHdr::BitmapHdr( uint32_t width, uint32_t height, Colorspace colorspace )
+BitmapHdr::BitmapHdr( uint32_t width, uint32_t height, Colorspace colorspace, int orientation )
     : m_width( width )
     , m_height( height )
     , m_data( new float[width*height*4] )
     , m_colorspace( colorspace )
+    , m_orientation( orientation )
 {
 }
 
@@ -86,6 +87,137 @@ void BitmapHdr::SetAlpha( float alpha )
         memcpy( ptr, &alpha, sizeof( float ) );
         ptr += 4;
     }
+}
+
+void BitmapHdr::NormalizeOrientation()
+{
+    if( m_orientation <= 1 ) return;
+
+    switch( m_orientation )
+    {
+    case 2:
+        FlipHorizontal();
+        break;
+    case 3:
+        Rotate180();
+        break;
+    case 4:
+        FlipVertical();
+        break;
+    case 5:
+        Rotate270();
+        FlipVertical();
+        break;
+    case 6:
+        Rotate90();
+        break;
+    case 7:
+        Rotate90();
+        FlipVertical();
+        break;
+    case 8:
+        Rotate270();
+        break;
+    default:
+        Panic( "Invalid orientation value!" );
+    }
+
+    m_orientation = 1;
+}
+
+void BitmapHdr::FlipVertical()
+{
+    auto ptr1 = m_data;
+    auto ptr2 = m_data + ( m_height - 1 ) * m_width * 4;
+    auto tmp = alloca( m_width * 4 * 4 );
+
+    for( uint32_t y=0; y<m_height/2; y++ )
+    {
+        memcpy( tmp, ptr1, m_width * 4 * 4 );
+        memcpy( ptr1, ptr2, m_width * 4 * 4 );
+        memcpy( ptr2, tmp, m_width * 4 * 4 );
+        ptr1 += m_width * 4;
+        ptr2 -= m_width * 4;
+    }
+}
+
+void BitmapHdr::FlipHorizontal()
+{
+    auto ptr = m_data;
+
+    for( uint32_t y=0; y<m_height; y++ )
+    {
+        auto ptr1 = ptr;
+        auto ptr2 = ptr + ( m_width - 1 ) * 4;
+
+        for( uint32_t x=0; x<m_width/2; x++ )
+        {
+            uint32_t tmp[4];
+            memcpy( tmp, ptr1, 4 * 4 );
+            memcpy( ptr1, ptr2, 4 * 4 );
+            memcpy( ptr2, tmp, 4 * 4 );
+            ptr1 += 4;
+            ptr2 -= 4;
+        }
+
+        ptr += m_width * 4;
+    }
+}
+
+void BitmapHdr::Rotate90()
+{
+    auto tmp = new float[m_width * m_height * 4];
+
+    auto src = m_data;
+    auto dst = tmp;
+
+    for( uint32_t y=0; y<m_height; y++ )
+    {
+        for( uint32_t x=0; x<m_width; x++ )
+        {
+            memcpy( dst + ( x * m_height + m_height - y - 1 ) * 4, src + ( y * m_width + x ) * 4, 4 * 4 );
+        }
+    }
+
+    delete[] m_data;
+    m_data = tmp;
+    std::swap( m_width, m_height );
+}
+
+void BitmapHdr::Rotate180()
+{
+    auto ptr1 = m_data;
+    auto ptr2 = m_data + ( m_width * m_height - 1 ) * 4;
+
+    for( uint32_t i=0; i<m_width * m_height / 2; i++ )
+    {
+        uint32_t tmp[4];
+        memcpy( tmp, ptr1, 4 * 4 );
+        memcpy( ptr1, ptr2, 4 * 4 );
+        memcpy( ptr2, tmp, 4 * 4 );
+        ptr1 += 4;
+        ptr2 -= 4;
+    }
+}
+
+void BitmapHdr::Rotate270()
+{
+    auto tmp = new float[m_width * m_height * 4];
+
+    auto src = m_data;
+    auto dst = tmp;
+
+    for( uint32_t y=0; y<m_height; y++ )
+    {
+        for( uint32_t x=0; x<m_width; x++ )
+        {
+            memcpy( dst + ( ( m_width - x - 1 ) * m_height + y ) * 4, src + ( y * m_width + x ) * 4, 4 * 4 );
+        }
+    }
+
+    delete[] m_data;
+    m_data = tmp;
+    std::swap( m_width, m_height );
 }
 
 std::unique_ptr<Bitmap> BitmapHdr::Tonemap( ToneMap::Operator op )
--- src/util/BitmapHdr.hpp
+++ src/util/BitmapHdr.hpp
@@ -13,18 +13,26 @@ class TaskDispatch;
 class BitmapHdr
 {
 public:
-    BitmapHdr( uint32_t width, uint32_t height, Colorspace colorspace );
+    BitmapHdr( uint32_t width, uint32_t height, Colorspace colorspace, int orientation = 0 );
     ~BitmapHdr();
     NoCopy( BitmapHdr );
 
     void Resize( uint32_t width, uint32_t height, TaskDispatch* td = nullptr );
     [[nodiscard]] std::unique_ptr<BitmapHdr> ResizeNew( uint32_t width, uint32_t height, TaskDispatch* td = nullptr ) const;
     void SetAlpha( float alpha );
+    void NormalizeOrientation();
+
+    void FlipVertical();
+    void FlipHorizontal();
+    void Rotate90();
+    void Rotate180();
+    void Rotate270();
 
     [[nodiscard]] uint32_t Width() const { return m_width; }
     [[nodiscard]] uint32_t Height() const { return m_height; }
     [[nodiscard]] float* Data() { return m_data; }
     [[nodiscard]] const float* Data() const { return m_data; }
+    [[nodiscard]] int Orientation() const { return m_orientation; }
     [[nodiscard]] Colorspace GetColorspace() const { return m_colorspace; }
 
     [[nodiscard]] std::unique_ptr<Bitmap> Tonemap( ToneMap::Operator op );
@@ -34,4 +42,6 @@ private:
     uint32_t m_height;
     float* m_data;
     Colorspace m_colorspace;
+
+    int m_orientation;
 };

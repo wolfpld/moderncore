f95878cdc09b9b49cad51040280572a953731450
Bartosz Taudul <wolf@nereid.pl>
2026-02-15 02:35:02 +0100

    Use helper function to create temporary ini files.

--- tests/util/Config.cpp
+++ tests/util/Config.cpp
@@ -1,28 +1,42 @@
 #include <catch2/catch_all.hpp>
 #include <src/util/Config.hpp>
+#include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 #include <unistd.h>
+
+// Helper to create a temp INI file with given content in current directory
+static std::string createTempIniFile( const char* content )
+{
+    char filename[256];
+    snprintf( filename, sizeof( filename ), "./test_config_%d_%ld.ini", getpid(), (long)time( nullptr ) );
+
+    FILE* f = fopen( filename, "w" );
+    if( !f ) return "";
+
+    if( content )
+    {
+        fprintf( f, "%s", content );
+    }
+
+    fclose( f );
+    return std::string( filename );
+}
 
 TEST_CASE( "Config functionality", "[config][ini]" )
 {
     SECTION( "Constructor with valid INI file" )
     {
-        char tempPath[256];
-        snprintf( tempPath, sizeof( tempPath ), "./test_config_valid_%d.ini", getpid() );
+        std::string tempPath = createTempIniFile(
+            "[test_section]\n"
+            "string_key=test_value\n"
+            "int_key=42\n"
+            "uint_key=256" );
+        REQUIRE( !tempPath.empty() );
 
-        FILE* f = fopen( tempPath, "w" );
-        REQUIRE( f != nullptr );
-        fprintf( f, "[test_section]\n"
-                    "string_key=test_value\n"
-                    "int_key=42\n"
-                    "uint_key=256" );
-        fclose( f );
-
-        Config config( tempPath );
+        Config config( tempPath.c_str() );
         REQUIRE( (bool)config );
 
-        unlink( tempPath );
+        unlink( tempPath.c_str() );
     }
 
     SECTION( "Constructor with invalid file path" )
@@ -33,18 +47,14 @@ TEST_CASE( "Config functionality", "[config][ini]" )
 
     SECTION( "Get with existing keys" )
     {
-        char tempPath[256];
-        snprintf( tempPath, sizeof( tempPath ), "./test_config_get_%d.ini", getpid() );
+        std::string tempPath = createTempIniFile(
+            "[test_section]\n"
+            "string_key=test_value\n"
+            "int_key=42\n"
+            "uint_key=256" );
+        REQUIRE( !tempPath.empty() );
 
-        FILE* f = fopen( tempPath, "w" );
-        REQUIRE( f != nullptr );
-        fprintf( f, "[test_section]\n"
-                    "string_key=test_value\n"
-                    "int_key=42\n"
-                    "uint_key=256" );
-        fclose( f );
-
-        Config config( tempPath );
+        Config config( tempPath.c_str() );
         auto strVal = config.Get( "test_section", "string_key", "default" );
         auto intVal = config.Get( "test_section", "int_key", 0 );
         auto uintVal = config.Get( "test_section", "uint_key", (uint32_t)0 );
@@ -53,21 +63,17 @@ TEST_CASE( "Config functionality", "[config][ini]" )
         REQUIRE( intVal == 42 );
         REQUIRE( uintVal == 256 );
 
-        unlink( tempPath );
+        unlink( tempPath.c_str() );
     }
 
     SECTION( "Get with missing keys returns defaults" )
     {
-        char tempPath[256];
-        snprintf( tempPath, sizeof( tempPath ), "./test_config_missing_%d.ini", getpid() );
+        std::string tempPath = createTempIniFile(
+            "[test_section]\n"
+            "existing_key=value" );
+        REQUIRE( !tempPath.empty() );
 
-        FILE* f = fopen( tempPath, "w" );
-        REQUIRE( f != nullptr );
-        fprintf( f, "[test_section]\n"
-                    "existing_key=value" );
-        fclose( f );
-
-        Config config( tempPath );
+        Config config( tempPath.c_str() );
         auto defaultStr = config.Get( "test_section", "missing_string_key", "default" );
         auto defaultInt = config.Get( "test_section", "missing_int_key", 999 );
         auto defaultUint = config.Get( "test_section", "missing_uint_key", (uint32_t)999 );
@@ -76,46 +82,38 @@ TEST_CASE( "Config functionality", "[config][ini]" )
         REQUIRE( defaultInt == 999 );
         REQUIRE( defaultUint == 999 );
 
-        unlink( tempPath );
+        unlink( tempPath.c_str() );
     }
 
     SECTION( "GetOpt with existing keys" )
     {
-        char tempPath[256];
-        snprintf( tempPath, sizeof( tempPath ), "./test_config_getopt_%d.ini", getpid() );
+        std::string tempPath = createTempIniFile(
+            "[test_section]\n"
+            "string_key=test_value" );
+        REQUIRE( !tempPath.empty() );
 
-        FILE* f = fopen( tempPath, "w" );
-        REQUIRE( f != nullptr );
-        fprintf( f, "[test_section]\n"
-                    "string_key=test_value" );
-        fclose( f );
-
-        Config config( tempPath );
+        Config config( tempPath.c_str() );
         const char* output;
         bool result = config.GetOpt( "test_section", "string_key", output );
         REQUIRE( result == true );
         REQUIRE( output != nullptr );
 
-        unlink( tempPath );
+        unlink( tempPath.c_str() );
     }
 
     SECTION( "GetOpt with missing keys" )
     {
-        char tempPath[256];
-        snprintf( tempPath, sizeof( tempPath ), "./test_config_getopt_missing_%d.ini", getpid() );
+        std::string tempPath = createTempIniFile(
+            "[test_section]\n"
+            "existing_key=value" );
+        REQUIRE( !tempPath.empty() );
 
-        FILE* f = fopen( tempPath, "w" );
-        REQUIRE( f != nullptr );
-        fprintf( f, "[test_section]\n"
-                    "existing_key=value" );
-        fclose( f );
-
-        Config config( tempPath );
+        Config config( tempPath.c_str() );
         const char* output;
         bool result = config.GetOpt( "test_section", "missing_key", output );
         REQUIRE( result == false );
 
-        unlink( tempPath );
+        unlink( tempPath.c_str() );
     }
 
     SECTION( "GetPath with ./ prefix" )
@@ -126,10 +124,8 @@ TEST_CASE( "Config functionality", "[config][ini]" )
 
     SECTION( "GetPath with environment variable" )
     {
-        // Save original environment variable
         const char* originalXdgConfig = getenv( "XDG_CONFIG_HOME" );
 
-        // Test with XDG_CONFIG_HOME unset
         if( originalXdgConfig )
         {
             unsetenv( "XDG_CONFIG_HOME" );
@@ -138,12 +134,10 @@ TEST_CASE( "Config functionality", "[config][ini]" )
         std::string path = Config::GetPath( "test.ini" );
         REQUIRE( path.find( "ModernCore/test.ini" ) != std::string::npos );
 
-        // Test with XDG_CONFIG_HOME set
         setenv( "XDG_CONFIG_HOME", "/tmp/test_config", 1 );
         std::string pathXdg = Config::GetPath( "test.ini" );
         REQUIRE( pathXdg == "/tmp/test_config/ModernCore/test.ini" );
 
-        // Restore original environment variable
         if( originalXdgConfig )
         {
             setenv( "XDG_CONFIG_HOME", originalXdgConfig, 1 );

5bd386f8c69c4b9cb26a5052bf020742e55e84bc
Bartosz Taudul <wolf@nereid.pl>
2026-02-22 14:44:56 +0100

    Properly handle mmap failure.

--- src/util/FileBuffer.cpp
+++ src/util/FileBuffer.cpp
@@ -21,7 +21,13 @@ FileBuffer::FileBuffer( const char* fn )
     m_size = ftell( file );
     fseek( file, 0, SEEK_SET );
 
-    m_data = (const char*)mmap( nullptr, m_size, PROT_READ, MAP_SHARED, fileno( file ), 0 );
+    auto map = mmap( nullptr, m_size, PROT_READ, MAP_SHARED, fileno( file ), 0 );
+    if( map == MAP_FAILED )
+    {
+        mclog( LogLevel::Error, "Failed to map file: %s", fn );
+        throw FileException( std::format( "Failed to map file: {}", fn ) );
+    }
+    m_data = (const char*)map;
 }
 
 FileBuffer::FileBuffer( FILE* file )
@@ -32,7 +38,13 @@ FileBuffer::FileBuffer( FILE* file )
     m_size = ftell( file );
     fseek( file, 0, SEEK_SET );
 
-    m_data = (const char*)mmap( nullptr, m_size, PROT_READ, MAP_SHARED, fileno( file ), 0 );
+    auto map = mmap( nullptr, m_size, PROT_READ, MAP_SHARED, fileno( file ), 0 );
+    if( map == MAP_FAILED )
+    {
+        mclog( LogLevel::Error, "Failed to map file" );
+        throw FileException( "Failed to map file" );
+    }
+    m_data = (const char*)map;
 }
 
 FileBuffer::FileBuffer( const std::shared_ptr<FileWrapper>& file )

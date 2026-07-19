33d6dbdf54349e3f9ea9a8e0bbdad8f676d40abf
Bartosz Taudul <wolf@nereid.pl>
2026-07-19 12:03:42 +0200

    Panic on failed embed decompress.

--- src/util/EmbedData.cpp
+++ src/util/EmbedData.cpp
@@ -1,11 +1,13 @@
 #include <lz4.h>
 
-#include "EmbedData.hpp"
+#include "util/EmbedData.hpp"
+#include "util/Panic.hpp"
 
 EmbedData::EmbedData( size_t size, size_t lz4Size, const uint8_t* data )
     : DataBuffer( new char[size], size )
 {
-    LZ4_decompress_safe( (const char*)data, (char*)m_data, lz4Size, size );
+    const auto dec = LZ4_decompress_safe( (const char*)data, (char*)m_data, lz4Size, size );
+    CheckPanic( dec == size, "Failed to decompress embedded data" );
 }
 
 EmbedData::~EmbedData()

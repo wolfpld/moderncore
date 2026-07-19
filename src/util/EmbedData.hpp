a29f628548072ee4e817286f77f2f2f2ab674550
Bartosz Taudul <wolf@nereid.pl>
2026-07-19 12:02:03 +0200

    EmbedData can't be copied.

--- src/util/EmbedData.hpp
+++ src/util/EmbedData.hpp
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include "util/DataBuffer.hpp"
+#include "util/NoCopy.hpp"
 
 #define Unembed( name ) auto name = std::make_shared<EmbedData>( Embed::name##Size, Embed::name##Lz4Size, Embed::name##Data )
 
@@ -12,4 +13,5 @@ class EmbedData : public DataBuffer
 public:
     EmbedData( size_t size, size_t lz4Size, const uint8_t* data );
     ~EmbedData() override;
+    NoCopy( EmbedData );
 };

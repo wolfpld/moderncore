6e13be1e5e76a174e83e33157bc66eaf34f0f3de
Bartosz Taudul <wolf@nereid.pl>
2026-08-01 19:05:17 +0200

    Use upstream defaults for max load factor

--- src/util/RobinHood.hpp
+++ src/util/RobinHood.hpp
@@ -24,11 +24,10 @@ using unordered_flat_set = std::unordered_set<_Value, _Hash, _Pred, _Alloc>;
 #include "contrib/robin_hood.h"
 
 template <typename Key, typename T, typename Hash = robin_hood::hash<Key>,
-          typename KeyEqual = std::equal_to<Key>, size_t MaxLoadFactor100 = 80>
+          typename KeyEqual = std::equal_to<Key>>
 using unordered_flat_map = robin_hood::unordered_flat_map<Key, T, Hash, KeyEqual>;
 
-template <typename Key, typename Hash = robin_hood::hash<Key>, typename KeyEqual = std::equal_to<Key>,
-          size_t MaxLoadFactor100 = 80>
-using unordered_flat_set = robin_hood::unordered_flat_set<Key, Hash, KeyEqual, MaxLoadFactor100>;
+template <typename Key, typename Hash = robin_hood::hash<Key>, typename KeyEqual = std::equal_to<Key>>
+using unordered_flat_set = robin_hood::unordered_flat_set<Key, Hash, KeyEqual>;
 
 #endif
